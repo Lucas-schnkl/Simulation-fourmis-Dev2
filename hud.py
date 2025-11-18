4962b2ee42de53e18b4ffd4de1cdde8fec6dc067
LeRoiJeanKins <mustafahs1659659@gmail.com>
2025-11-18 21:33:59 +0100

    Refactor hud.py to integrate Nid and Fourmis
    Refactor hud.py to include Nid and Fourmis classes, and adjust the initialization of the food sources and the Nid position.

--- hud.py
+++ hud.py
@@ -1,5 +1,10 @@
 import tkinter as tk
-import fourmies
+import fourmis
+from Nid_fichier import Nid
+import random
+from time import sleep
+
+from fourmis import Fourmis
 
 fenetre = tk.Tk()
 fenetre.geometry("700x700")          #taimlle fenetre
@@ -10,27 +15,33 @@ canvas.pack()
 
 pos_depart_x, pos_depart_y = 0,0
 taille_pixel = 15
-couleur_pixel = "#F5CC00"
-bordure = "white"
-canvas.create_rectangle(pos_depart_x, pos_depart_y, taille_pixel, taille_pixel, fill=couleur_pixel, outline=bordure) # structure d'un pixel
 taille =700 // taille_pixel #le nombre de case par lignes
+
+#canvas.create_rectangle(pos_depart_x, pos_depart_y, taille_pixel, taille_pixel, fill=couleur_pixel, outline=bordure) # structure d'un pixel
+
 
 grille_phero = []
 for y in range(taille):
     lignes = []
     for x in range(taille):
-        lignes.append(fourmies.Pheromones())
+        lignes.append(fourmis.Pheromones())
     grille_phero.append(lignes)
 
 nbr_source_nourriture = 5 #choix du nombre de source qui appariassent sur la carte
 source=[]
+nid_noir = Nid(0,0)
+
+THE_GOAT_OF_FOURMIES = Fourmis()
+
+canvas.create_rectangle(nid_noir.pos_x, nid_noir.pos_y, nid_noir.taille, nid_noir.taille, fill=nid_noir.couleur, outline=nid_noir.couleur)
+
 for _ in range(nbr_source_nourriture):
     while True:
         x = random.randint(0,taille-1)
         y = random.randint(0,taille-1)
-        if (x,y) != (pos_depart_x,pos_depart_y) and (x,y) != (nid.pos_x,nid.pos_y):
+        if (x,y) != (pos_depart_x,pos_depart_y) and (x,y) != (nid_noir.pos_x,nid_noir.pos_y):
             break
-    s=fourmies.SourceNourriture(pos_x=x,pos_y=y)
+    s=fourmis.SourceNourriture(pos_x=x,pos_y=y)
     source.append(s)
 for s in source:
     canvas.create_rectangle(
