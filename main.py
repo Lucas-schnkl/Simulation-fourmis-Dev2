e09546a7b71c090c00c6838d6d281bfda9bbc8d7
Lucas-schnkl <lucas.schenkel12@gmail.com>
2025-11-25 20:42:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 from environnement import Environnement
 from simulation import Simulation
-from Nid_fichier import Nid
+from nid import Nid
 from fourmis import Ouvriere, Soldat, Reine
 from prédateurs import Predateur
 from source_nourriture import SourceNourriture
 from interface_graphique import InterfaceGraphique
 
-def main():
-    # Création environnement
-    env = Environnement(largeur=100, hauteur=100)
 
+# Création environnement
+env = Environnement(largeur=100, hauteur=100)
+
+def main():
     #Création interface graphique
     interface = InterfaceGraphique(env)
 
     # Création du nid
-    nid = Nid(50, 50)
+    nid = Nid(30, 30)
     env.ajouter_nid(nid)
 
     # Ajouter fourmis
     for _ in range(50):
         env.ajouter_fourmi(Ouvriere(vie=100, pos_x=50, pos_y=50, pheromones="", statut="vivante", nourriture=100))
 
-    env.ajouter_fourmi(Reine(vie=100, statut="vivante", pheromones="", pos_x=50, pos_y=50, nourriture=100))
-    env.ajouter_fourmi(Soldat(pos_x=50, pos_y=50))
+    env.ajouter_fourmi(Reine(vie=100, statut="vivante", pheromones="", pos_x=40, pos_y=40, nourriture=100))
+    env.ajouter_fourmi(Soldat(pos_x=55, pos_y=55))
 
     # Ajouter sources nourriture
     env.ajouter_source(SourceNourriture(10, 20, quantite=200))
