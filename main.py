54eb3f1970aab358f340e29a26c10e3b90e8e84f
Lucas-schnkl <lucas.schenkel12@gmail.com>
2025-12-11 10:37:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from source_nourriture import SourceNourriture
 from predateur import Predateur
 from simulation import Simulation
 from fourmis import Fourmis
+from resultats import resultats
 import random
 
 
@@ -11,40 +12,47 @@ import random
 env = Environnement(largeur=100, hauteur=100)
 
 def main():
-    # Création du nid
-    nid = Nid(envi=env, pos_x=50, pos_y=50)
-    env.ajouter_nid(nid)
+    try:
+        # Création du nid
+        nid = Nid(envi=env, pos_x=50, pos_y=50)
+        env.ajouter_nid(nid)
 
-    # Ajouter fourmis
-    for _ in range(50):
-        env.ajouter_fourmi(Fourmis(envi = env,pos_x=50, pos_y=50, pheromones=""))
+        # Ajouter fourmis
+        for _ in range(50):
+            env.ajouter_fourmi(Fourmis(envi = env,pos_x=50, pos_y=50, pheromones=""))
 
-    #env.ajouter_fourmi(Reine(envi = env, vie=100, statut="vivante", pheromones="", pos_x=40, pos_y=40, nourriture=100))
-    #env.ajouter_fourmi(Soldat(envi = env, pos_x=55, pos_y=55))
+        #env.ajouter_fourmi(Reine(envi = env, vie=100, statut="vivante", pheromones="", pos_x=40, pos_y=40, nourriture=100))
+        #env.ajouter_fourmi(Soldat(envi = env, pos_x=55, pos_y=55))
 
-    # Ajouter sources nourriture
-    nbr_sources = 5
-    taille_env = 100
+        # Ajouter sources nourriture
+        nbr_sources = 5
+        taille_env = 100
 
-    for _ in range(nbr_sources):
-        while True:
-            x = random.randint(0, taille_env - 1)
-            y = random.randint(0, taille_env - 1)
+        for _ in range(nbr_sources):
+            while True:
+                x = random.randint(0, taille_env - 1)
+                y = random.randint(0, taille_env - 1)
 
-            if (x, y) != (30, 30): # spawn pas sur le nid
-                break
+                if (x, y) != (30, 30): # spawn pas sur le nid
+                    break
 
-        env.ajouter_source(SourceNourriture(envi=env, pos_x=x, pos_y=y, compteur=3))
+            env.ajouter_source(SourceNourriture(envi=env, pos_x=x, pos_y=y, compteur=3))
 
-    # Ajouter prédateurs
-    env.ajouter_predateur(Predateur(envi=env, pos_x=70, pos_y=70))
+        # Ajouter prédateurs
+        env.ajouter_predateur(Predateur(envi=env, pos_x=70, pos_y=70))
 
-    # Lancer simulation
-    sim = Simulation(env)
-    sim.boucle()
+        # Lancer simulation
+        sim = Simulation(env)
+        sim.boucle()
 
-    # garde la fenêtre de la simulation ouverte
-    #interface.fenetre.mainloop()
+        # garde la fenêtre de la simulation ouverte
+        #interface.fenetre.mainloop()
+
+    except:
+        print("Simulation arrêtée manuellement")
+
+    finally:
+        resultats(env)
 
 
 if __name__ == "__main__":
