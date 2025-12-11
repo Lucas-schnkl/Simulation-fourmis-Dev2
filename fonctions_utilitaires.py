a378ac534ac753fd899b1e803d976a23af41015a
LeRoiJeanKins <mustafahs1659659@gmail.com>
2025-12-11 10:38:35 +0100

    Add files via upload

---
+++ fonctions_utilitaires.py
@@ -0,0 +1,12 @@
+def start():
+    global cours_utilisation
+    cours_utilisation = True
+
+    while cours_utilisation:
+        """ à completer avec le code qui joue la simulation """
+
+def stop():
+    cours_utilisation = False
+
+def sauvegarde():
+    print("pas encore fait")
