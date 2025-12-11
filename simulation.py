95f8b1860b930c8c889f8c2c8f6938f463fe7e77
Lucas-schnkl <lucas.schenkel12@gmail.com>
2025-12-11 10:37:31 +0100

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -6,31 +6,32 @@ class Simulation:
 
     # Boucle infinie pour la simulation
     def boucle(self):
-        if not self.en_cours:
-            return
+        while self.en_cours:
+            if not self.en_cours:
+                return
 
-        # dessin état statique
-        #self._interface.dessiner_static()
+            # dessin état statique
+            #self._interface.dessiner_static()
 
-        # dessin sources de nourriture
-        #self._interface.dessiner_sources()
+            # dessin sources de nourriture
+            #self._interface.dessiner_sources()
 
-        # déplacement fourmis
-        for f in self.env.fourmis:
-            f.se_deplacer(self.env)
-            #f.trouve_nourriture(self.env.sources)
+            # déplacement fourmis
+            for f in self.env.fourmis:
+                f.se_deplacer(self.env)
+                #f.trouve_nourriture(self.env.sources)
 
-        # déplacement prédateurs
-        for p in self.env.predateurs:
-            p.se_deplacer()
+            # déplacement prédateurs
+            for p in self.env.predateurs:
+                p.se_deplacer()
 
-        # evaporation
-        #self.env.evaporation()
+            # evaporation
+            #self.env.evaporation()
 
-        # update interface
-        #self._interface.update()
+            # update interface
+            #self._interface.update()
 
-        # relancer la boucle dans 30 ms acr tkinter aime pas boucle infini donc fait équivalent
-        #self._interface.fenetre.after(30, self.boucle)
+            # relancer la boucle dans 30 ms acr tkinter aime pas boucle infini donc fait équivalent
+            #self._interface.fenetre.after(30, self.boucle)
 
-        print("Une boucle de la simulation a tourné")
+            print("Une boucle de la simulation a tourné")
